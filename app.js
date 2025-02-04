67b8a6b8359a2f8c8f7e35ee1bef83ae88f5265c
GabrielGomezGG <43209264+GabrielGomezGG@users.noreply.github.com>
2025-02-04 17:12:58 -0300

    Eliminar nombres iniciales de la lista de amigos y ajustar la lógica de validación

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 
-let names = ["titi", "pepe", "ñeresad"]
+let names = []
 const listaAmigos = document.getElementById("listaAmigos")
 const textField = document.getElementById("amigo")
 const resultado = document.getElementById("resultado")
-
-actualizarLista()
 
 function isEmptyName(name) {    
     if (name == 0) {
