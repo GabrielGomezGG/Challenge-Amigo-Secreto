9fa602dd09cd1fed753e7d2085ef250dcf556bf9
GabrielGomezGG <43209264+GabrielGomezGG@users.noreply.github.com>
2025-02-04 17:00:58 -0300

    Agregar funcionalidad de "Amigo Secreto" con interfaz y lógica para sortear amigos

---
+++ README.md
@@ -0,0 +1,5 @@
+## Amigo secreto
+
+### Descripción
+
+Este proyecto vas a poder ingresar nombres de amigos y el sistema te va a devolver un amigo aleatoreo.
---
+++ app.js
@@ -0,0 +1,51 @@
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+let names = []
+const listaAmigos = document.getElementById("listaAmigos")
+const textField = document.getElementById("amigo")
+const resultado = document.getElementById("resultado")
+
+function isEmptyName(name) {    
+    if (name == 0) {
+        alert("El campo nombre no puede estar vacío")
+        return true
+    }
+    return false
+}
+
+function actualizarLista() {
+    listaAmigos.innerHTML = ""
+    names.forEach(name => {
+        const li = document.createElement("li")
+        li.textContent = name
+        listaAmigos.appendChild(li)
+    })
+
+    textField.value = ""
+}
+
+function agregarAmigo() {
+
+    const name = document.getElementById("amigo").value
+
+    if (!isEmptyName(name)) {
+        names.push(name)
+
+        actualizarLista()
+    }
+}
+
+function sortearAmigo(){
+    if(names.length != 0){
+        let amigoSorteado = names[Math.floor(Math.random() * names.length)]
+        resultado.innerHTML = "El amigo secreto es: " + amigoSorteado
+
+        names = []
+        listaAmigos.innerHTML = ""
+    }
+}
+
+
+
+
+
